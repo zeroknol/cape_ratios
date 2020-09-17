0a0ae754a96d3e53023c6bdb447f5c12a00172b3
Amit Chakradeo <amit@chakradeo.net>
2020-09-17 11:02:21 -0700

    Drop existing views for updates

--- scraper.py
+++ scraper.py
@@ -12,6 +12,8 @@ sqlite_table = "data"
 df.to_sql(sqlite_table, sqlite_connection, if_exists='replace')
 
 # Set up views for easier exports
+sqlite_connection.execute('DROP VIEW IF EXISTS "countries"')
 sqlite_connection.execute('CREATE VIEW "countries" AS select * from "data" where ("Score" is not NULL) ORDER BY Score ASC limit 40')
+sqlite_connection.execute('DROP VIEW IF EXISTS "regions"')
 sqlite_connection.execute('CREATE VIEW "regions" AS SELECT "Country" AS "Region", "Weight", "CAPE", "PE", "PC", "PB", "PS", "DY", "RS 26W", "RS 52W" from "data" WHERE ("index" > "0") AND ("Score" IS NULL) ORDER BY "Weight DESC"')
 sqlite_connection.close()
