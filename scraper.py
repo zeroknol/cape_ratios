018258bce9237a6c29bc497a410b43ecd8c477b4
Amit Chakradeo <amit@chakradeo.net>
2020-09-16 23:13:26 -0700

    Initial

--- scraper.py
+++ scraper.py
@@ -1,16 +1,22 @@
 # This is a template for a Python scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
+import scraperwiki
+import requests
+import pandas as pd
+from sqlalchemy import create_engine
+
+r = requests.get('https://www.starcapital.de/fileadmin/charts/Res_Aktienmarktbewertungen_FundamentalKZ_Tbl.php?lang=en')
+j = r.json()
+df = pd.DataFrame([[d['v'] for d in x['c']] for x in j['rows']], columns=[d['label'] for d in j['cols']])
+
+engine = create_engine('sqlite:///data.sqlite', echo=True)
+sqlite_connection = engine.connect()
+sqlite_table = "data"
+df.to_sql(sqlite_table, sqlite_connection, if_exists='replace')
+sqlite_connection.close()
+
+
 # # Write out to the sqlite database using scraperwiki library
 # scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
 #
