a6092831cb8104d3ee9596a56978fdbdc15decf8
Amit Chakradeo <amit@chakradeo.net>
2020-09-17 10:43:43 -0700

    Try python3 runtime for kicks

--- scraper.py
+++ scraper.py
@@ -13,5 +13,5 @@ df.to_sql(sqlite_table, sqlite_connection, if_exists='replace')
 
 # Set up views for easier exports
 sqlite_connection.execute('CREATE VIEW "countries" AS select * from "data" where ("Score" is not NULL) ORDER BY Score ASC limit 40')
-sqlite_connection.execute('CREATE VIEW "regions" AS SELECT "Country", "Weight", "CAPE", "PE", "PC", "PB", "PS", "DY", "RS 26W", "RS 52W" from "data" WHERE ("index" > "0") AND ("Score" IS NULL) ORDER BY "Weight DESC"')
+sqlite_connection.execute('CREATE VIEW "regions" AS SELECT "Country" AS "Region", "Weight", "CAPE", "PE", "PC", "PB", "PS", "DY", "RS 26W", "RS 52W" from "data" WHERE ("index" > "0") AND ("Score" IS NULL) ORDER BY "Weight DESC"')
 sqlite_connection.close()
